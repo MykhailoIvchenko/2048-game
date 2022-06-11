d75eee16c074667e86bf5ef602025c83f3f6b749
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-06-11 22:31:28 +0300

    Readme edited

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-  extends: '@mate-academy/eslint-config',
+  extends: ['@mate-academy/eslint-config',
+    'plugin:cypress/recommended'],
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    "requireConfigFile" : "false",
+    babelOptions: {
+      "configFile": "./node_modules/@parcel/fs/src/.babelrc",
+    }
+  }
 };
